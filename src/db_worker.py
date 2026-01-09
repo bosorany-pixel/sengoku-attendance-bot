2884d4678032205675ce0758360c85070dcc5171
Zkh-dot <kernzahar@gmail.com>
2026-01-09 11:55:05 +0000

    pupupu

--- src/db_worker.py
+++ src/db_worker.py
@@ -236,7 +236,7 @@ VALUES (?, ?)
     def get_balance(self, uid) -> float:
         rows = self.fetchall(
             """
-            select p.payment_ammount * 1.0 / p.user_amount
+            select (p.payment_ammount * 1.0) / (p.user_amount * 1.0)
             from PAYMENTS p
             join PAYMENTS_TO_USERS ul on p.message_id = ul.message_id
             where ul.ds_uid = ?
