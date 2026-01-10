ffa2b846fc90f5ad40eaacc508a9f1074b9c7387
Zkh-dot <kernzahar@gmail.com>
2026-01-10 00:06:38 +0000

    fix loc_bot

--- objects_tracker/main.py
+++ objects_tracker/main.py
@@ -61,7 +61,11 @@ bot.tree.add_command(payments.inc_payment)
 bot.tree.add_command(payments.dec_payment)
 bot.tree.add_command(payments.get_balance)
 bot.tree.add_command(add_from_image)
-bot.add_listener(payments.on_message, name="on_message", bot=bot)
+bot.add_listener(
+    lambda message: payments.on_message(message, bot),
+    name="on_message"
+)
+
 
 @tasks.loop(minutes=1)
 async def cleanup_data_loop():
